45adfca4393deb8bc8a0acd09f4dc5416b9f364a
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-01-25 21:44:22 +0100

    Failing test for adding task in timesheet and asking for statistics #3

--- internal/db/repositoryimpl_test.go
+++ internal/db/repositoryimpl_test.go
@@ -9,6 +9,7 @@ import (
 
 	. "github.com/jborkows/timesheets/internal/db"
 	. "github.com/jborkows/timesheets/internal/model"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestShouldBeAbleToDisplayStatisticsForNoneData(t *testing.T) {
@@ -22,6 +23,69 @@ func TestShouldBeAbleToDisplayStatisticsForNoneData(t *testing.T) {
 		if len(statistics) != 0 {
 			t.Errorf("Expected none statistic, got %d", len(statistics))
 		}
+	})
+}
+
+func TestShouldBeAbleToPresentDailyStatisticsPendingThenSave(t *testing.T) {
+	useDb(t, func(saver Saver, query Queryer) {
+		timesheet := TimesheetForDate(time.Now())
+
+		task := "work"
+		entry := TimesheetEntry{Hours: 4, Minutes: 0, Category: "work", Comment: "work", Task: &task}
+		if error := timesheet.AddEntry(entry); error != nil {
+			t.Errorf("Error adding entry: %v", error)
+		}
+		saveError := saver.PendingSave(timesheet)
+		if saveError != nil {
+			t.Errorf("Error pending saving timesheet: %v", saveError)
+		}
+		statistics, err := query.Daily(timesheet)
+		if err != nil {
+			t.Errorf("Error getting daily statistics: %v", err)
+		}
+		assert.Equal(t, 1, len(statistics), "Expected 1 entry statistic, got %d", len(statistics))
+		stat := statistics[0]
+		assert.Equal(t, 4, stat.Dirty.Hours, "Expected 4 hours, got %d", stat.Dirty.Hours)
+		assert.Equal(t, 0, stat.Daily.Hours, "Expected 0 hours, got %d", stat.Daily.Hours)
+
+		saveError = saver.Save(timesheet)
+		if saveError != nil {
+			t.Errorf("Error saving timesheet: %v", saveError)
+		}
+
+		statistics, err = query.Daily(timesheet)
+		if err != nil {
+			t.Errorf("Error getting daily statistics: %v", err)
+		}
+		assert.Equal(t, 1, len(statistics), "Expected 1 entry statistic, got %d", len(statistics))
+		stat = statistics[0]
+		assert.Equal(t, 4, stat.Dirty.Hours, "Expected 4 hours, got %d", stat.Dirty.Hours)
+		assert.Equal(t, 4, stat.Daily.Hours, "Expected 4 hours, got %d", stat.Daily.Hours)
+	})
+}
+
+func TestShouldBeAbleToPresentDailyStatisticsSave(t *testing.T) {
+	useDb(t, func(saver Saver, query Queryer) {
+		timesheet := TimesheetForDate(time.Now())
+
+		task := "work"
+		entry := TimesheetEntry{Hours: 4, Minutes: 0, Category: "work", Comment: "work", Task: &task}
+		if error := timesheet.AddEntry(entry); error != nil {
+			t.Errorf("Error adding entry: %v", error)
+		}
+		saveError := saver.Save(timesheet)
+		if saveError != nil {
+			t.Errorf("Error pending saving timesheet: %v", saveError)
+		}
+		statistics, err := query.Daily(timesheet)
+		if err != nil {
+			t.Errorf("Error getting daily statistics: %v", err)
+		}
+		assert.Equal(t, 1, len(statistics), "Expected 1 entry statistic, got %d", len(statistics))
+		stat := statistics[0]
+		assert.Equal(t, 4, stat.Dirty.Hours, "Expected 4 hours, got %d", stat.Dirty.Hours)
+		assert.Equal(t, 4, stat.Daily.Hours, "Expected 0 hours, got %d", stat.Daily.Hours)
+
 	})
 }
 
