20e32accf9797dc0350488d94569c047761bfba7
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-01-25 21:28:05 +0100

    useDB function to create each time new database and run migration

--- internal/db/repositoryimpl_test.go
+++ internal/db/repositoryimpl_test.go
@@ -1,6 +1,9 @@
 package db_test
 
 import (
+	"fmt"
+	"log"
+	"os"
 	"testing"
 	"time"
 
@@ -9,16 +12,38 @@ import (
 )
 
 func TestShouldBeAbleToDisplayStatisticsForNoneData(t *testing.T) {
-	t.Parallel()
-	repository := Repository()
-	var query Queryer = repository
-	timesheet := TimesheetForDate(time.Now())
+	useDb(t, func(saver Saver, query Queryer) {
+		timesheet := TimesheetForDate(time.Now())
 
-	statistics, err := query.Daily(timesheet)
+		statistics, err := query.Daily(timesheet)
+		if err != nil {
+			t.Errorf("Error getting daily statistics: %v", err)
+		}
+		if len(statistics) != 0 {
+			t.Errorf("Expected none statistic, got %d", len(statistics))
+		}
+	})
+}
+
+func useDb(t *testing.T, test func(saver Saver, querier Queryer)) {
+	t.Parallel()
+	tempFile, err := os.CreateTemp("", "testdb-*.db")
+	defer func() {
+		tempFile.Close()
+
+		err = os.Remove(tempFile.Name())
+		if err != nil {
+			fmt.Println("Error removing temporary file:", err)
+		} else {
+			fmt.Println("Temporary file removed.")
+		}
+	}()
+
+	_, err = NewDatabase(tempFile.Name())
 	if err != nil {
-		t.Errorf("Error getting daily statistics: %v", err)
+		log.Fatalf("Failed to initialize database: %v", err)
 	}
-	if len(statistics) != 0 {
-		t.Errorf("Expected none statistic, got %d", len(statistics))
-	}
+
+	repository := Repository()
+	test(repository, repository)
 }
