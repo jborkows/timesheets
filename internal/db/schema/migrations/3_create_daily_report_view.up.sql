ee6aada6dfd8c9d342f0fe29b344393f2e75ba85
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-02-13 00:07:55 +0100

    Fix function floor not recognized #3

--- internal/db/schema/migrations/3_create_daily_report_view.up.sql
+++ internal/db/schema/migrations/3_create_daily_report_view.up.sql
@@ -3,8 +3,8 @@ select t.date,
     te.pending,
     te.category,
     te.holiday,
-    sum(te.hours) + floor(sum(te.minutes)/60) as hours,
-    floor(mod(te.Minutes,60)) minutes 
+    sum(te.hours) + Round(sum(te.minutes)/60,0) as hours,
+    te.Minutes%60 minutes 
 from timesheet_data t
 join timesheet_entry_data te on t.date = te.timesheet_date
 group by t.date, te.pending, te.holiday, te.category;
