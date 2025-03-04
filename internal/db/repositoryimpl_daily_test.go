98c89e1699bae18b148f6ef2cb9b7128d21d715a
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-03-04 18:32:11 +0100

    Renaming scripts, name matters, fixing wrong group by

--- internal/db/repositoryimpl_daily_test.go
+++ internal/db/repositoryimpl_daily_test.go
@@ -87,3 +87,39 @@ func TestShouldBeAbleToPresentDailyStatisticsSave(t *testing.T) {
 
 	})
 }
+
+func TestShouldBeAbleToPresentDailyStatisticsOrderShouldNotMatter(t *testing.T) {
+	useDb(t, func(saver model.Saver, query model.Queryer) {
+		timesheet := model.TimesheetForDate(time.Now())
+
+		task := "work"
+		entry := model.TimesheetEntry{Hours: 4, Minutes: 20, Category: "work", Comment: "work", Task: &task}
+		if error := timesheet.AddEntry(entry); error != nil {
+			t.Errorf("Error adding entry: %v", error)
+		}
+		entry = model.TimesheetEntry{Hours: 4, Minutes: 30, Category: "work", Comment: "work", Task: &task}
+		if error := timesheet.AddEntry(entry); error != nil {
+			t.Errorf("Error adding entry: %v", error)
+		}
+		entry = model.TimesheetEntry{Hours: 4, Minutes: 30, Category: "work_reversed", Comment: "work", Task: &task}
+		if error := timesheet.AddEntry(entry); error != nil {
+			t.Errorf("Error adding entry: %v", error)
+		}
+		entry = model.TimesheetEntry{Hours: 4, Minutes: 20, Category: "work_reversed", Comment: "work", Task: &task}
+		if error := timesheet.AddEntry(entry); error != nil {
+			t.Errorf("Error adding entry: %v", error)
+		}
+		saveError := saver.Save(context.Background(), timesheet)
+		if saveError != nil {
+			t.Errorf("Error pending saving time sheet: %v", saveError)
+		}
+		statistics, err := query.Daily(context.Background(), timesheet)
+		if err != nil {
+			t.Errorf("Error getting daily statistics: %v", err)
+		}
+		for _, stat := range statistics {
+			assert.Equal(t, uint8(8), stat.Daily.Hours, "Expected 8 hours, got %d", stat.Daily.Hours)
+			assert.Equal(t, uint8(50), stat.Daily.Minutes, "Expected 50 minutes, got %d", stat.Daily.Minutes)
+		}
+	})
+}
--- internal/db/schema/migrations/006_create_monthly_report_view.up.sql
+++ internal/db/schema/migrations/006_create_monthly_report_view.up.sql
@@ -4,7 +4,7 @@ select te.month,
     te.category,
     te.holiday,
     sum(te.hours) + Round(sum(te.minutes)/60,0) as hours,
-    te.Minutes%60 minutes 
+    sum(te.Minutes)%60 minutes 
 from timesheet_data t
 join timesheet_entry_data te on t.date = te.timesheet_date
 group by te.month, te.pending, te.holiday, te.category;
