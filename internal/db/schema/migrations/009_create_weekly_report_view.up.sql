1308aed0a4cbbed9d9249ac715e58891596ebc59
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-03-08 03:23:02 +0100

    Fix create weekly report view

--- internal/db/schema/migrations/009_create_weekly_report_view.up.sql
+++ internal/db/schema/migrations/009_create_weekly_report_view.up.sql
@@ -5,7 +5,7 @@ select t.week_begin_date,
     te.category,
     te.holiday,
     sum(te.hours) + Round(sum(te.minutes)/60,0) as hours,
-    te.Minutes%60 minutes 
+    sum(te.Minutes)%60 minutes 
 from timesheet_data t
 join timesheet_entry_data te on t.date = te.timesheet_date
 group by t.week_begin_date, t.week_end_date, te.pending, te.holiday, te.category;
